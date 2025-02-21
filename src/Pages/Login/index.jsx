bc192a8fef9e36a8f4fe0e55a4b3fbe35febe5e7
vishallmaurya <vishallmaurya210@gmail.com>
2025-02-21 20:10:34 +0530

    added axios and connected to backend

--- src/Pages/Login/index.jsx
+++ src/Pages/Login/index.jsx
@@ -1,35 +1,58 @@
-import { useForm } from "react-hook-form"
+import { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
 import styles from "./Login.module.css";
-import { useGoogleLogin  } from '@react-oauth/google';
+import { useGoogleLogin } from "@react-oauth/google";
+import { getBackendURL } from "../../utils/EnvLoader.js";
+import axios from "axios";
 
 export const Login = () => {
     const { handleSubmit, reset, register } = useForm();
+    const [loginData, setLoginData] = useState({ email: "", password: "" });
+    const [gmailLogin, setGmailLogin] = useState({ isGmailLogin: false });
+    const [triggerSend, setTriggerSend] = useState(false);  
+    
+    useEffect(() => {
+        if (triggerSend) {
+            sendData();
+            setTriggerSend(false);  
+        }
+    }, [loginData, gmailLogin]);  
+    
+    const sendData = async () => {
+        try {
+            let data = { ...loginData, ...gmailLogin };
+            console.log("Sending Data:", data);
+            const response = await axios.post(getBackendURL() + "/users/register", data, { withCredentials: true });
+            console.log(response);
+        } catch (error) {
+            console.error("Error sending data:", error);
+        }
+    };
 
     const onSubmit = (data) => {
-        console.log(data);
+        console.log("Form Data:", data);
+        setLoginData(data);
+        setTriggerSend(true);  
         reset();
-    }
+    };
 
     const handleGoogleLoginSuccess = async (tokenResponse) => {
         const accessToken = tokenResponse.access_token;
 
         try {
-            const response = await fetch(
-                "https://www.googleapis.com/oauth2/v2/userinfo",
-                {
-                    headers: {
-                        Authorization: `Bearer ${accessToken}`,
-                    },
-                }
-            );
+            const response = await fetch("https://www.googleapis.com/oauth2/v2/userinfo", {
+                headers: { Authorization: `Bearer ${accessToken}` },
+            });
 
             const user = await response.json();
             console.log("User Info:", user);
+
+            setGmailLogin({ isGmailLogin: true });
+            setTriggerSend(true);  
         } catch (error) {
             console.error("Error fetching user info:", error);
         }
     };
-    
 
     const handleGoogleLoginFailure = () => {
         console.log("Google Login Failed");
@@ -40,26 +63,22 @@ export const Login = () => {
         onError: handleGoogleLoginFailure,
     });
 
-
     return (
         <div className={`${styles["login-container"]} ${styles["align"]}`}>
-            <h2 style={{marginBottom: "3rem"}}>Login to Analyzer</h2>
+            <h2 style={{ marginBottom: "3rem" }}>Login to Analyzer</h2>
             <form onSubmit={handleSubmit(onSubmit)} className={`${styles["login-form-container"]} ${styles["align"]}`}>
                 <input type="text" placeholder="e.g. abc@gmail.com" {...register("email")} className={styles["login-input"]} />
-                <input type="password" placeholder="123@password" {...register("pwd")}  className={styles["login-input"]} />
-                <button  className={`${styles["login-input"]} ${styles["login-btn"]}`}>Login</button>
+                <input type="password" placeholder="123@password" {...register("password")} className={styles["login-input"]} />
+                <button className={`${styles["login-input"]} ${styles["login-btn"]}`}>Login</button>
             </form>
             <div className={`${styles["partition"]}`}>
                 <hr />
                 or
                 <hr />
             </div>
-            <button 
-                className={`${styles["login-input"]} ${styles["login-btn"]}`} 
-                onClick={() => googleLogin()}
-            >
+            <button className={`${styles["login-input"]} ${styles["login-btn"]}`} onClick={() => googleLogin()}>
                 Login with Google
             </button>
         </div>
-    )
-}
+    );
+};
--- src/utils/EnvLoader.js
+++ src/utils/EnvLoader.js
@@ -1,3 +1,7 @@
 export function getGoogleClientId() {
     return import.meta.env.VITE_GOOGLE_CLIENT_ID;
 }
+
+export function getBackendURL() {
+    return import.meta.env.VITE_BACKEND_URL;
+}
