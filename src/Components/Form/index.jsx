e05721ff1aed242552a29d5b0d4385a5c2fe93c3
vishallmaurya <vishallmaurya210@gmail.com>
2025-03-19 12:50:31 +0530

    added loading state

--- src/Components/Form/index.jsx
+++ src/Components/Form/index.jsx
@@ -2,25 +2,49 @@ import { useEffect } from "react";
 import styles from "./Form.module.css";
 import { useForm } from "react-hook-form";
 import { useState } from "react";
+import axios from "axios";
+import { getBackendURL } from "../../utils/EnvLoader.js";
 
 export const Form = () => {
     const { handleSubmit, register, reset } = useForm();
-    const [data, setData] = useState("");
+    const [output, setOutput] = useState("");
+    const [loading, setLoading] = useState(false);
 
     const onSubmit = (data) => {
-        console.log(data);
-        setData(data.data);
+        if (data.tweet?.trim() === "") {
+            reset();
+            return;
+        }
+        
+        sendData(data);
+        
         reset(); 
+    };
+
+    const sendData = async (data) => {
+        try {
+            setLoading(true);
+            const response = await axios.post(getBackendURL() + "/api/predict", data, { withCredentials: true });
+            setOutput(response.data.data.predicted_class);
+        } catch (error) {
+            console.error("Error sending data:", error);
+        } finally {
+            setLoading(false);
+        }
     };
 
     return (
         <div className={styles["form-container"]}>
             <form onSubmit={handleSubmit(onSubmit)} className={styles["form"]}>
-                <input type="text"  {...register("data")} placeholder="enter you tweet here..." className={styles["form-input"]}/>
+                <input type="text"  {...register("tweet")} placeholder="enter you tweet here..." className={styles["form-input"]}/>
                 <button className={styles["form-btn"]} >Sentiment</button>
             </form>
+            {loading && <div className={styles["output"]}>
+                <img src="/loader.gif" alt="Loading..." />
+            </div>}
             <div className={styles["output"]}>
-                {data}
+                {console.log(output)}
+                {output}
             </div>
         </div>
     )
