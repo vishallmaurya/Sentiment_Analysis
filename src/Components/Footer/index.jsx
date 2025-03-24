1e5b54f45c059484094b88849a557aff6336a1c0
vishallmaurya <vishallmaurya210@gmail.com>
2025-03-24 14:34:37 +0530

    home page responsiveness done

--- src/Components/Footer/index.jsx
+++ src/Components/Footer/index.jsx
@@ -3,15 +3,17 @@ import styles from "./Footer.module.css";
 export const Footer = () => {
     return (
         <div className={ styles["footer-container"]}>
-            <h4>
-                © 2025 Tweet Analyzer. All Rights Reserved.
-            </h4>
-            <div className={styles["media-links"]}>
-                links {/* to do */}
+            <div className={ styles["footer-container-child"]}>
+                <h4>
+                    © 2025 Tweet Analyzer. All Rights Reserved.
+                </h4>
+                <div className={styles["media-links"]}>
+                    links {/* to do */}
+                </div>
+                <p>
+                    Powered by AI & NLP | Built by Vishal Maurya
+                </p>
             </div>
-            <p>
-                Powered by AI & NLP | Built by Vishal Maurya
-            </p>
         </div>
     )
 }
