3b32e8bcaabbb06ac4325095b9fbca96de819742
vishallmaurya <vishallmaurya210@gmail.com>
2025-03-26 19:40:19 +0530

    path changed

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,15 @@ import { AppLayout } from "./layout/AppLayout"
 import { getGoogleClientId } from "../src/utils/EnvLoader.js";
 import { GoogleOAuthProvider } from '@react-oauth/google';
 
-const Home = lazy(() => import("./Pages/Home"));
-const Login = lazy(() => import("./Pages/Login"));
-const Profile = lazy(() => import("./Pages/Profile"));
-const ResetPassword = lazy(() => import("./Pages/ResetPassword/index.jsx"));
+// const Home = lazy(() => import("./Pages/Home"));
+// const Login = lazy(() => import("./Pages/Login"));
+// const Profile = lazy(() => import("./Pages/Profile"));
+// const ResetPassword = lazy(() => import("./Pages/ResetPassword/index.jsx"));
+
+import { Home } from './Pages/Home'
+import { Login } from './Pages/Login'
+import { Profile } from './Pages/Profile'
+import { ResetPassword } from './Pages/ResetPassword/index.jsx'
 
 function App() {
   const router = createBrowserRouter([
